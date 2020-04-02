f815919cf6cd0f1ea63e2604c98d87c1dd9cd851
Ilya Krioni <ilya.krioni@finch-vr.com>
2020-04-02 19:23:28 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace find_the_cat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
